9af3336b8cdd4b1f48725ccd6e552a06de3b504f
“Mahmoud <mahmoudAB93@hotmail.com>
2016-11-06 09:58:13 -0500

    fixing format

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+
+session_start();
+
+$user = isset($_POST['user']) ? $_POST['user'] : '';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['submit']) &&  $_POST['user'] == ''){
+        echo "Username is empty!";
+  } 
+	  elseif (isset($_POST['submit']) && (isset($_POST['pass']) & $_POST['pass'] == '')) {
+        echo "Password is empty!";
+  }
+else {
+
+    if(isset($_POST['user'])){
+        $username= $_POST['user'];
+    }
+
+    if(isset($_POST['pass'])){
+        $pass= $_POST['pass'];
+    }
+		$mysqli = new mysqli("localhost", "root", "root", "database_initial");
+    
+	if(mysqli_connect_error()){
+
+		echo"Cannot connect to the database";
+	}
+
+//Mysql query to find username and password
+//$query = "SELECT * FROM `users` WHERE `name` = '".$username."'";
+
+//
+$query = "SELECT * FROM `users` WHERE `password` = '".$pass."' AND `username` = '".$username."'";
+$result = $mysqli->query($query);
+if ($result->num_rows > 0) {
+   
+   header('Location: welcome.php'); // redirect to requested page and logged in
+
+    
+} else {
+  echo "failed to login";
+}
+
+/* close connection */
+$mysqli->close();
+	}
+	
+}
+?>
+<html>
+<head>
+<title>Home</title>
+</head>
+<body>
+
+<h1>Student Management center</h1>
+
+<form action="" method="post">
+  login:<br>
+   <input type="text" name="user" id="user" value="<?php echo $user; ?>" /><br><br>
+  <br>
+  password:<br>
+  <input type="password" name="pass" id="pass" value="" /><br><br>
+  <br><br>
+ <input type="submit" value="Login" name="submit" id="submit" />
+
+ <a href="register.php">
+   <button>Register</button>
+</a>
+  
+
+</form>
+
+
+</body>
+</html>
